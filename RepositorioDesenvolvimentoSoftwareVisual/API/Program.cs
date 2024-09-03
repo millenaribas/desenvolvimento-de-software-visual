cece242ea2005ef7349ce1b9ed9c3b3880af1f35
millenaribas <millenaguimaraes.1@hotmail.com>
2024-09-02 21:46:20 -0300

    atualizando dados e atividade da aula

--- RepositorioDesenvolvimentoSoftwareVisual/API/Program.cs
+++ RepositorioDesenvolvimentoSoftwareVisual/API/Program.cs
@@ -1,6 +1,21 @@
+//Minimal APIs em C#
+//Testar a API:
+// - Rest Client - Extensão VSCode
+// - Postman
+// - Insomia
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//EndPoint - Funcionalidade da aplicação
+//Requisição - URL e método/verbo HTTP
+//app.MapGet("/qualquercoisa", () => "Hello World!");
+app.MapGet("/", () => "Hello World em C#!");
+
+app.MapGet("/segundafuncionalidade", () => "Segunda funcionalidade!");
+
 
 app.Run();
+
+//Exercicios para próxima aula
+//Criar um endpoint para receber informação pela URL
+//Receber a informação pelo corpo
